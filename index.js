d232451a11f1598af30b7e5093c7ac3e9bf1b479
Luiz Flores Goncalves Neto <luizfloresgoncalvesneto@gmail.com>
2023-08-18 21:52:39 -0300

    criado operadores-e-logica-de-programacao

---
+++ index.js
@@ -0,0 +1,25 @@
+// exercicio interpretacao de codigo
+
+// exer 1
+
+
+// a. vai aparecer falso pois nao são iguais
+
+// b. vai aparecer falso pois nenhum vai ser igual o outro
+
+// c. vai aparecer true pois o inverso do primeiro com o segundo ou o terceiro da true 
+
+// d. vai aparecer op tipo que é boleano
+
+//exer 2 : ele esta considerando como string 
+
+/*exer 3 : falta o Number antes da escrita primeiroNumero e segundoNumero
+const soma = Number(primeiroNumero) + Number(segundoNumero)*/
+
+//exercicio de escrita de código
+
+//exer 1 
+const idadeUsuario = prompt("Qual é sua idade usuario? ")
+const idadeAmigo = prompt("Qual é a idade do seu melhor amigo? ")
+const comparacao = idadeUsuario >= idadeAmigo
+console.log(comparacao)
