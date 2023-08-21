89525d94d0630eaf0dcb9fabcdcca849204a1efa
Luiz Flores Goncalves Neto <luizfloresgoncalvesneto@gmail.com>
2023-08-21 20:07:47 -0300

    criado operadores-e-logica-de-programacao

--- index.js
+++ index.js
@@ -19,7 +19,24 @@ const soma = Number(primeiroNumero) + Number(segundoNumero)*/
 //exercicio de escrita de código
 
 //exer 1 
-const idadeUsuario = prompt("Qual é sua idade usuario? ")
-const idadeAmigo = prompt("Qual é a idade do seu melhor amigo? ")
-const comparacao = idadeUsuario >= idadeAmigo
-console.log(comparacao)
+/*const idadeUsuario = prompt("Qual é sua idade usuario? ") resp: a
+const idadeAmigo = prompt("Qual é a idade do seu melhor amigo? ") resp:  b
+const comparacao = idadeUsuario >= idadeAmigo resp : c
+const diferenca = Number(idadeUsuario) - Number(idadeAmigo) resp: d
+console.log(comparacao)  true pois eu tenho mais idade do que ele resp: c
+console.log(diferenca) aparece a diferença que é 14 anos entre as idades resp : d */
+
+//exer 2
+
+/*const numeroPar = prompt("Insira um numero par ")// resp: a
+const resultado = Number(numeroPar) % 2// resp: b
+console.log(resultado) // resp: c padrão tudo 0 //
+//resp : d vai aparecer o número 1 */
+
+/*const idadeUsuario = prompt("Qual sua idade? ")
+const idadeMeses = Number(idadeUsuario) * 12
+const idadeDias = Number(idadeMeses) * 30
+const idadeHoras =Number(idadeDias) * 24
+console.log(idadeMeses)
+console.log(idadeDias)
+console.log(idadeHoras)*/
